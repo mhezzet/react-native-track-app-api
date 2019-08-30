dc17bd9dd24b92685a20905213bbc493a195c9a7
mhezzet <40916821+mhezzet@users.noreply.github.com>
2019-08-30 18:50:41 +0200

    added gcloud app.yaml

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 
 module.exports = function(req, res, next) {
   const { authorization } = req.headers
-  if (!authorization) res.status(401).send('u r not logged in')
+  if (!authorization) return res.status(401).send('u r not logged in')
   const token = authorization.replace('Bearer ', '')
 
   jwt.verify(token, '123', async (err, payload) => {
